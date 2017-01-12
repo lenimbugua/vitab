258502832fdba764132313ad089c1d6584894978
Amos Omondi <amos.omondi@andela.com>
2017-01-12 11:19:33 +0300

    (feat) refactor models, add serializer file

--- vitab/api/models.py
+++ vitab/api/models.py
@@ -1,3 +1,34 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+from cloudinary.models import CloudinaryField
+
+
+class Base(models.Model):
+    name = models.CharField(max_length=40)
+    date_added = models.DateField(auto_now_add=True, default=datetime.datetime.now())
+    creator = models.ForeignKey(User, on_delete=Cascade)
+
+    class Meta:
+        abstract = True
+
+
+class Photo(Base):
+    image = CloudinaryField()
+
+    def __str__(self):
+        return "{}".format(self.image.name)
+
+    def save(self, *args, **kwargs):
+        name = super(Photo, self).image.name
+
+        self.save()
+
+
+class Video(Base):
+    pass
+
+
+class Contest(Base):
+    pass
